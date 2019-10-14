20b0f0872ac9dbb7aa4b7eb8aa82b04ccb7b760c
nisahani <nurrochmah.hani98@gmail.com>
2019-10-14 09:10:28 +0700

    Initial commit

---
+++ app/src/main/java/com/nisa/aplikasidoa/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.nisa.aplikasidoa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void postLogin(View view) {
+        Intent intent = new Intent(this, MainMenuActivity.class);
+        startActivity(intent);
+    }
+}
