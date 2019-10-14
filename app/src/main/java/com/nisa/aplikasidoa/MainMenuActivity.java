bddeeab52121733381d3b59772453393c28131ee
nisahani <nurrochmah.hani98@gmail.com>
2019-10-14 09:20:56 +0700

    kritik saran

--- app/src/main/java/com/nisa/aplikasidoa/MainMenuActivity.java
+++ app/src/main/java/com/nisa/aplikasidoa/MainMenuActivity.java
@@ -2,7 +2,9 @@ package com.nisa.aplikasidoa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void clickKritikSaran(View view) {
+        Intent i = new Intent(MainMenuActivity.this, KritikSaranActivity.class);
+        startActivity(i);
     }
 }
